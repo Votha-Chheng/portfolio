26a40b8704570fb2afa80f9dbdc5d923bcf1c4aa
Votha-Chheng <votha-chheng@users.noreply.github.com>
2021-08-31 21:51:59 +0200

    beta

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const back = document.querySelector(".back")
-const front = document.querySelector(".front")
-console.log(front.getBoundingClientRect())
-console.log(front.getBoundingClientRect().right - front.getBoundingClientRect().left)
+
+
+
